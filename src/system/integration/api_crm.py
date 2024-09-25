d6e3b8eec1c097db48e3db32108c2cc012dee766
Manoel Messias <manoelmsnsi@gmail.com>
2024-09-25 10:55:41 -0300

    fix

--- src/system/integration/api_crm.py
+++ src/system/integration/api_crm.py
@@ -110,15 +110,43 @@ class ApiBackend():
 
     async def get_pessoa(self,filters:dict,token = None):
         try:
-            # self.auth()
             url = f"{self.BASE_URL}/pessoa"
-            headers={"Authorization":token}
-            payload=filters
+            headers = {"Authorization": token}
+            payload = filters
+            payload["size"]= 100
+            page = 1
+            all_data = []
+            response_data = {
+            "items": [],
+            "total": 0,
+            "page": 1,
+            "size": 50,
+            "pages": 1
+        }
             
             async with httpx.AsyncClient() as client:
-                response = await client.request(method="GET",headers=headers,url=url,params=payload)
-            response.raise_for_status()    
-            return response.json()
+                while True:
+                    payload["page"] = page  # Define o número da página no payload
+                    resp = await client.get(url, headers=headers, params=payload)
+                    resp.raise_for_status()
+                    json_response = resp.json()
+
+                    # Adiciona os itens da página atual
+                    response_data["items"].extend(json_response.get("items", []))
+
+                    # Preenche os demais campos de controle apenas na primeira iteração
+                    if page == 1:
+                        response_data["total"] = json_response.get("total", 0)
+                        response_data["size"] = json_response.get("size", 50)
+                        response_data["pages"] = json_response.get("pages", 1)
+
+                    # Verifica se já processou todas as páginas
+                    if page >= response_data["pages"]:
+                        break
+
+                    page += 1  # Próxima página
+
+            return response_data  # Retorna a estrutura com
         
         except Exception as error:
             print(f"backend -> get_pessoa -> [ {error} ]")
@@ -786,6 +814,7 @@ class ApiBackend():
             url = f"{self.BASE_URL}/empresa"
             headers={"Authorization":token}
             payload=filters
+            payload["size"]=100
             async with httpx.AsyncClient() as client:
                 response = await client.request(method="GET",headers=headers,url=url,params=payload)
             response.raise_for_status()    
